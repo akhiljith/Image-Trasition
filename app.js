cba609006b3eae74fb73a7d8e29227262d99a223
akhiljith <akhiljith.c.a@gmail.com>
2020-12-04 22:46:41 +0530

    transition workng

---
+++ app.js
@@ -0,0 +1,8 @@
+  new hoverEffect({
+    parent: document.querySelector('.distortion'),
+    intensity: 0.3,
+    image1: 'https://images.squarespace-cdn.com/content/v1/572f75e77c65e4a1135a3266/1500055386885-9VI5BNQ5ZTL2FNMDGLF6/ke17ZwdGBToddI8pDm48kAr75V_jkPVL5j5tPB6Hp457gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QHyNOqBUUEtDDsRWrJLTmzUsryC7riGV7bTeYhg5SehXJLTdBuLqhTDeFLgsHvdY5xKmava-rqefwd0XGBik6/image-asset.jpeg?format=1000w',
+    image2: 'https://images.squarespace-cdn.com/content/v1/572f75e77c65e4a1135a3266/1500056102581-ADJ2N02B6CHC1ZPII51K/ke17ZwdGBToddI8pDm48kAT1x_N7fM1j3cIAm1oQBdZZw-zPPgdn4jUwVcJE1ZvWQUxwkmyExglNqGp0IvTJZamWLI2zvYWH8K3-s_4yszcp2ryTI0HqTOaaUohrI8PIMXpG68wAI520BbtcjKchG86wj6d6fSOaOUJhOJlUQzQKMshLAGzx4R3EDFOm1kBS/image-asset.jpeg?format=1000w',
+    displacementImage: 'https://raw.githubusercontent.com/robin-dela/hover-effect/master/images/heightMap.png',
+    crossOrigin: null
+});
